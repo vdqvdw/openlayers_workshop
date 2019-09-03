ea622e22020590351bfae5f186873e346c4b1789
박지훈 <jjjhhhvvv@aipers.com>
2019-09-03 23:31:28 +0900

    [vertor-data] 다운로드 기능 구현 (json)    GeoJson 포맷으로 데이터 파일 다운로드

--- src/en/main.js
+++ src/en/main.js
@@ -5,6 +5,7 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
 import DragAndDrop from 'ol/interaction/DragAndDrop';
+import Modify from 'ol/interaction/Modify'
 
 const map = new Map({
     target: 'map-container',
@@ -25,13 +26,33 @@ const layer = new VectorLayer({
 // add Layer
 map.addLayer(layer);
 
-// aadd Interaction
+// add Interaction Drag ang Drop
 map.addInteraction(new DragAndDrop({
     source: source,
     formatConstructors: [GeoJSON]
 }));
 
+// add Interaction Modify
+map.addInteraction(new Modify({
+    source: source
+}));
 
 
+// clear feature
+const clearBtn = document.getElementById('clear');
+
+clearBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    source.clear();
+});
+
+// download feature
+const format = new GeoJSON({featureProjection: 'EPSG:3857'});
+const downloadBtn = document.getElementById('download');
+source.on('change', function() {
+    const features = source.getFeatures();
+    const json = format.writeFeatures(features);
+    downloadBtn.href = 'data:text/json;charset=utf-8,' + json;
+});
 
 
