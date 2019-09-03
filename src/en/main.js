51298dbc25a79b14b47e194b0b8f1ffef08962bc
박지훈 <jjjhhhvvv@aipers.com>
2019-09-03 22:29:07 +0900

    [vertor-data] 지도의 경계선 수정하기

--- src/en/main.js
+++ src/en/main.js
@@ -1,1 +1,39 @@
-alert('Hello Workshop');
+import 'ol/ol.css';
+import GeoJSON from 'ol/format/GeoJSON';
+import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import View from 'ol/View';
+import DragAndDrop from 'ol/interaction/DragAndDrop';
+import Modify from 'ol/interaction/Modify';
+import sync from 'ol-hashed';
+
+// create source and layer
+const source = new VectorSource();
+const layer = new VectorLayer({
+    source: source
+});
+const map = new Map({
+    target: 'map-container',
+    view: new View({
+        center: [0, 0],
+        zoom: 2
+    })
+});
+
+// add Layer
+map.addLayer(layer);
+
+// drag amd drop interaction
+map.addInteraction(new DragAndDrop({
+    source: source,
+    formatConstructors: [GeoJSON]
+}));
+
+// add Interaction and Modifying features
+map.addInteraction(new Modify({
+    source: source
+}));
+
+sync(map);
+
