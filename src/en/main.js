171fc8910e0a1d2196b71d2d35853caae8ed065d
박지훈 <jjjhhhvvv@gmail.com>
2019-09-03 14:41:27 +0900

    [vector_data]  GeoJson 데이터 드래그 앤 드롭하여 렌더링 기능 구현

--- src/en/main.js
+++ src/en/main.js
@@ -1,1 +1,37 @@
-alert('Hello Workshop');
+import 'ol/ol.css';
+import GeoJSON from 'ol/format/GeoJSON';
+import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import View from 'ol/View';
+import DragAndDrop from 'ol/interaction/DragAndDrop';
+
+const map = new Map({
+    target: 'map-container',
+    view: new View({
+        center: [0, 0],
+        zoom: 2
+    })
+});
+
+// empty VectorSource
+const source = new VectorSource();
+
+// create VectorLayer and has a source
+const layer = new VectorLayer({
+    source: source
+});
+
+// add Layer
+map.addLayer(layer);
+
+// aadd Interaction
+map.addInteraction(new DragAndDrop({
+    source: source,
+    formatConstructors: [GeoJSON]
+}));
+
+
+
+
+
